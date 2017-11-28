08a59bd611c216a9fbc631cfb5270f1d3735f89e
leiliu0718 <31353159+leiliu0718@users.noreply.github.com>
2017-11-27 20:56:22 -0600

    Update cluster.py

--- a4/cluster.py
+++ a4/cluster.py
@@ -87,6 +87,7 @@ def main():
     print('Actual number of users per community:',[len(sub) for sub in subs])
     
     print('Save the communities.')
+    pickle.dump(subs, open('graph.pickle', 'wb'))
     pickle.dump(subs, open('subs.pickle', 'wb'))
     
 
